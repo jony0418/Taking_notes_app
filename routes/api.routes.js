3e50da3d5226006d0b708a2e204759c85b95beca
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-05-30 23:00:13 -0600

    Changed all my base code to try other way to do it base on an example found online

---
+++ routes/api.routes.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = (app) => {
+  app.get("/api/notes", function(req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
+
+  app.post("/api/notes", function(req, res) {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      });
+    });
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    const id = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+      notes = notes.filter((note) => note.id !== id);
+      fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        if (err) throw err;
+        res.json({ message: "Note deleted" });
+      });
+    });
+  });
+};
