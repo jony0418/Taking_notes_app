1edfbbf3efa40067346718ea0a31be8c2c372c8b
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-05-30 22:19:51 -0600

    Add the installation of the modules

---
+++ Develop/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid'); // For generating unique IDs for notes
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// GET route for returning notes
+app.get("/api/notes", function(req, res) {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+// POST route for saving notes
+app.post("/api/notes", function(req, res) {
+  const newNote = req.body;
+  newNote.id = uuidv4(); // Assign a unique id to the note
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+// DELETE route for deleting notes
+app.delete("/api/notes/:id", function(req, res) {
+  const id = req.params.id;
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== id);
+    fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
+      if (err) throw err;
+      res.json({ message: "Note deleted" });
+    });
+  });
+});
+
+// HTML routes
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
