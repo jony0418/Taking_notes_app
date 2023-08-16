756400fe434d48e3e673dcad58f699622ad2d74c
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-08-15 22:28:21 -0600

    edditing the commentaries

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,12 +1,10 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 
-console.log('api.routes.js is being run');
-
 module.exports = (app) => {
   app.get("/api/notes", function(req, res) {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
+      if (err) return res.status(500).json({ error: err });
       res.json(JSON.parse(data));
     });
   });
@@ -15,11 +13,11 @@ module.exports = (app) => {
     const newNote = req.body;
     newNote.id = uuidv4();
     fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
+      if (err) return res.status(500).json({ error: err });
       const notes = JSON.parse(data);
       notes.push(newNote);
       fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
-        if (err) throw err;
+        if (err) return res.status(500).json({ error: err });
         res.json(newNote);
       });
     });
@@ -28,15 +26,13 @@ module.exports = (app) => {
   app.delete("/api/notes/:id", function(req, res) {
     const id = req.params.id;
     fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
+      if (err) return res.status(500).json({ error: err });
       let notes = JSON.parse(data);
       notes = notes.filter((note) => note.id !== id);
       fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
-        if (err) throw err;
+        if (err) return res.status(500).json({ error: err });
         res.json({ message: "Note deleted" });
       });
     });
   });
 };
-
-
